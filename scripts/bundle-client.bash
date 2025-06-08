119e598792378a6a584d0109227e05d8c16a552a
Kamil Trzciński <ayufan@ayufan.eu>
2025-06-08 15:34:36 +0200

    Add v3.4.1
    This updates release process, and used dockerfiles.

--- scripts/bundle-client.bash
+++ scripts/bundle-client.bash
@@ -2,14 +2,11 @@
 
 DESTDIR=/
 NAME=proxmox-backup-client-$VERSION
-BINARIES=( /src/proxmox-backup/target/release/{dump-catalog-shell-cli,pxar,proxmox-backup-client} )
+BINARIES=( /src/proxmox-backup/target/static-build/*/release/{pxar,proxmox-backup-client} )
 
-mkdir -p "$DESTDIR/$NAME" "$DESTDIR/$NAME/lib"
+mkdir -p "$DESTDIR/$NAME"
 
 cp -v ${BINARIES[@]} "$DESTDIR/$NAME"
-ldd ${BINARIES[@]} | grep "=> /" | awk '{print $3}' | sort -u | xargs -I '{}' cp -v '{}' "$DESTDIR/$NAME/lib"
-cp /lib/ld-linux*.so* "$DESTDIR/$NAME/lib"
-
 cp -v /scripts/client/* "$DESTDIR/$NAME"
 
 cd $DESTDIR
