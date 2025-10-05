24552476923350c5c8ef2f41f76e43dab176edad
Marcin Maj <marcinmajsc@gmail.com>
2025-10-05 02:33:23 +0200

    Update git-clone.bash
    Added configurable primary/fallback clone bases so build tooling can switch to HTTPS when the git:// protocol is unavailable.

--- scripts/build/git-clone.bash
+++ scripts/build/git-clone.bash
@@ -8,8 +8,14 @@ if [[ $# -ne 1 ]] && [[ $# -ne 2 ]]; then
 fi
 
 perform() {
+  local repo_name="${3:-$1}"
+  local primary_base="${GIT_CLONE_PRIMARY:-git://git.proxmox.com/git}"
+  local fallback_base="${GIT_CLONE_FALLBACK:-https://git.proxmox.com/git}"
+
   if [[ ! -d "$1" ]]; then
-    git clone "git://git.proxmox.com/git/${3:-$1}.git" "$1"
+    if ! git clone "${primary_base}/${repo_name}.git" "$1"; then
+      git clone "${fallback_base}/${repo_name}.git" "$1"
+    fi
   else
     git -C "$1" fetch
   fi
@@ -23,7 +29,15 @@ perform() {
   ( cd "$1"; git ls-files -z | xargs -0 touch -h )
 }
 
-while read REPO COMMIT_SHA REST; do
+while true; do
+  REPO=""
+  COMMIT_SHA=""
+  REST=""
+
+  if ! read REPO COMMIT_SHA REST; then
+    [[ -n "$REPO" ]] || break
+  fi
+  
   [[ -n "$2" ]] && [[ "$REPO" != "$2" ]] && continue
   echo "$REPO $COMMIT_SHA..." 1>&2
   REPO_URL=${REST%%#*}
